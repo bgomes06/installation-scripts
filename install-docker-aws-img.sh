1baccdd70bd28d6ad7d9d534fb5a02a8f733acf3
EC2 Default User <ec2-user@ip-172-31-26-121.ec2.internal>
2019-10-16 19:24:00 +0000

    Adding docker/compose script for AWS LINUX AMI

---
+++ install-docker-aws-img.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo amazon-linux-extras install docker -y
+
+sleep 2
+
+sudo service docker start
+sudo service docker enable
+
+sudo usermod -a -G docker ec2-user
+
+sleep 2
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
